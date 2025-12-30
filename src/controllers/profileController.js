3720d2bffe5268d8ae6a40669d33feddf5c3c0a9
AMIT DHATTERWAL <amitchitain25@gmail.com>
2025-12-30 18:33:01 +0530

    profileUpdate api change

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,27 +1,38 @@
 import User from "../models/User.js";
+import fs from "fs";
+import path from "path";
 
 // update profile
 export const updateProfile = async (req, res) => {
-    try {
-        const { id } = req.user;
-        const { fullName,  countryCode, phone, sports } = req.body;
-        const user = await User.findById(id);
+  try {
+    // console.log("BODY:", req.body);
+    // console.log("FILE:", req.file);
 
-        const updateData = {
-            fullName,
-            countryCode,
-            phone,
-            sports: sports ? JSON.parse(sports) : user.sports,
-        };
-
-        if (req.file) {
-            updateData.profileImage = req.file.path || req.file.filename;
-        }
-
-        const updated = await User.findByIdAndUpdate(id, updateData, { new: true });
-        return res.json({ msg: "Profile updated", user: updated });
-
-    } catch (err) {
-        return res.status(500).json({ msg: "Error updating profile" });
+    const { userId, fullName, phone, countryCode } = req.body;
+    if (!userId) {
+      return res.status(400).json({ msg: "User ID missing" });
     }
+
+    let updateData = { fullName, phone, countryCode };
+
+    if (req.file) {
+      updateData.profileImage = `/uploads/profile/${req.file.filename}`;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    return res.json({ msg: "Profile Updated Successfully", user: updatedUser });
+  } catch (error) {
+    console.error("Update ERROR:", error);
+    return res.status(500).json({ msg: "Error updating profile" });
+  }
 };
+
